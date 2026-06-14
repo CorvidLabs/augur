8a088ab513d492faa2840e5c6cb5014c943d7d6e
Leif <leif.algo@pm.me>
2026-06-14 10:46:03 -0600

    Update hub.ts

--- site/src/lib/hub.ts
+++ site/src/lib/hub.ts
@@ -1,3 +1,3 @@
 // Destinations on the CorvidLabs hub that supersede this retired site.
-export const HUB_MARKETING = 'https://corvidlabs.github.io/corvidlabs-site/augur/'
-export const HUB_DOCS = 'https://corvidlabs.github.io/corvidlabs-site/augur/docs/'
+export const HUB_MARKETING = 'https://corvidlabs.xyz/augur/'
+export const HUB_DOCS = 'https://corvidlabs.xyz/augur/docs/'
